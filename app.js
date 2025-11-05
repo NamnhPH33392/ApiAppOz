e61429c7a67ed82779be3d5270613ed39315a9b4
Nam Nguyen <namnhph33392@fpt.edu.vn>
2025-11-05 16:32:49 +0700

    p2

--- app.js
+++ app.js
@@ -1,41 +1,49 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import cookieParser from "cookie-parser";
+import path from "path";
+import { fileURLToPath } from "url";
+import createError from "http-errors";
+import "./models/db.js"; // Kết nối MongoDB
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const app = express();
 
-var app = express();
+// Cấu hình view (nếu bạn vẫn muốn giữ EJS)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-app.use(logger('dev'));
+app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// ✅ Tạm thời comment các router cũ để không lỗi
+// import indexRouter from './routes/index.js';
+// import usersRouter from './routes/users.js';
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
+// Test route
+app.get("/", (req, res) => {
+  res.send("✅ Server đang chạy và MongoDB đã kết nối!");
+});
+
+// Bắt lỗi 404
+app.use((req, res, next) => {
   next(createError(404));
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+// Xử lý lỗi tổng
+app.use((err, req, res, next) => {
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
+  res.locals.error = req.app.get("env") === "development" ? err : {};
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
-module.exports = app;
+export default app;
